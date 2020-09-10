970edb2b36a70bb9a25f663b0cd82cbeee5c193b
rishabh-as <Predict@123>
2020-09-10 18:07:50 +0530

    changing pin numbers to match PCM

--- library/tray/tray_up.py
+++ library/tray/tray_up.py
@@ -3,7 +3,7 @@ import RPi.GPIO as gpio
 import json
 
 config_file = open("../../config.json", 'r')
-configuration = json.loads(config_file.read())["tray"]["tray_up"]
+configuration = json.loads(config_file.read())["tray"]
 print(configuration)
 
 motor_moving = False
@@ -25,8 +25,8 @@ def setup():
 	gpio.output(configuration['motor_2_direction_pin'], configuration['clockwise'])
 
 	# setting up cut switches
-	gpio.setup(15, gpio.IN, pull_up_down=gpio.PUD_DOWN)  # top switch
-	gpio.setup(18, gpio.IN, pull_up_down=gpio.PUD_DOWN)  # bottom Switch
+	gpio.setup(configuration['top_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # top switch
+	gpio.setup(configuration['bottom_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # bottom Switch
 
 
 def motor_rotate():
@@ -39,14 +39,14 @@ def motor_rotate():
 
 
 def start_condition():
-	if gpio.input(15) == gpio.LOW and gpio.input(18) == gpio.HIGH:
+	if gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
 		return True
 	else:
 		return False
 
 
 def stop_condition():
-	if gpio.input(15) == gpio.HIGH and gpio.input(18) == gpio.LOW:
+	if gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
 		return False
 	else:
 		return True
