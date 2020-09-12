34d70d611c5b24c1582c4ffa60c95df26ba6d924
rishabh-as <Predict@123>
2020-09-12 15:48:08 +0530

    fixing food tray use code

--- library/food_tray/food_tray_use.py
+++ library/food_tray/food_tray_use.py
@@ -31,15 +31,6 @@ def setup():
 	gpio.setup(configuration['top_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # top switch
 	gpio.setup(configuration['bottom_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # bottom Switch
 
-
-def motor_rotate():
-	gpio.output(configuration['motor_1_step_pin'], gpio.HIGH)
-	gpio.output(configuration['motor_2_step_pin'], gpio.HIGH)
-	sleep(.0002)
-	gpio.output(configuration['motor_1_step_pin'], gpio.LOW)
-	gpio.output(configuration['motor_2_step_pin'], gpio.LOW)
-	sleep(.0002)
-
 def distance():
 	# set Trigger to HIGH
 	gpio.output(configuration['ultrasonic_trigger'], True)
@@ -68,15 +59,24 @@ def distance():
 	return distance
 
 
+def motor_rotate():
+	gpio.output(configuration['motor_1_step_pin'], gpio.HIGH)
+	gpio.output(configuration['motor_2_step_pin'], gpio.HIGH)
+	sleep(.0002)
+	gpio.output(configuration['motor_1_step_pin'], gpio.LOW)
+	gpio.output(configuration['motor_2_step_pin'], gpio.LOW)
+	sleep(.0002)
+
+
 def start_condition():
-	if gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
+	if gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
 		return True
 	else:
 		return False
 
 
 def stop_condition():
-	if gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
+	if gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
 		return False
 	else:
 		return True
@@ -84,7 +84,13 @@ def stop_condition():
 
 if __name__ == '__main__':
 	setup()
+	print("setup completed")
 	while True:
+		print("entered main program")
 		while start_condition():
+			print("condition 1 success")
 			while stop_condition():
+				print("condition 2 success")
 				motor_rotate()
+			print("condition 2 failed")
+		print("condition 1 failed")
