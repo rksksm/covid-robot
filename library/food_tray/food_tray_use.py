09fc4fd5fef615a89ae6caf54ea049694ff79cd0
rishabh-as <Predict@123>
2020-09-12 15:21:33 +0530

    adding food_tray code

--- library/food_tray/food_tray_use.py
+++ library/food_tray/food_tray_use.py
@@ -1,4 +1,5 @@
 from time import sleep
+import time
 import RPi.GPIO as gpio
 import json
 
@@ -6,13 +7,15 @@ config_file = open("../../config.json", 'r')
 configuration = json.loads(config_file.read())["tray"]
 print(configuration)
 
-motor_moving = False
-
 
 def setup():
 	# setting up the RaspberryPi modes
 	gpio.setwarnings(False)  # Ignore warning for now
 	gpio.setmode(gpio.BCM)  # Use physical pin numbering
+
+	# setting up Ultrasonic sensor
+	gpio.setup(configuration['ultrasonic_trigger'], gpio.OUT)
+	gpio.setup(configuration['ultrasonic_echo'], gpio.IN)
 
 	# setting up the motor 1
 	gpio.setup(configuration['motor_1_direction_pin'], gpio.OUT)
@@ -36,6 +39,34 @@ def motor_rotate():
 	gpio.output(configuration['motor_1_step_pin'], gpio.LOW)
 	gpio.output(configuration['motor_2_step_pin'], gpio.LOW)
 	sleep(.0002)
+
+
+def distance():
+	# set Trigger to HIGH
+	gpio.output(configuration['ultrasonic_trigger'], True)
+
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	gpio.output(configuration['ultrasonic_trigger'], False)
+
+	StartTime = time.time()
+	StopTime = time.time()
+
+	# save StartTime
+	while gpio.input(configuration['ultrasonic_echo']) == 0:
+		StartTime = time.time()
+
+	# save time of arrival
+	while gpio.input(configuration['ultrasonic_echo']) == 1:
+		StopTime = time.time()
+
+	# time difference between start and arrival
+	TimeElapsed = StopTime - StartTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	distance = (TimeElapsed * 34300) / 2
+
+	return distance
 
 
 def start_condition():
