d102a9a5bf241c097cfdf86b7a5810523f2c69b1
rishabh-as <Predict@123>
2020-09-12 16:14:26 +0530

    fixing ultrasound bug in food tray

--- library/food_tray/food_tray_refill.py
+++ library/food_tray/food_tray_refill.py
@@ -1,18 +1,21 @@
 from time import sleep
+import time
 import RPi.GPIO as gpio
 import json
 
 config_file = open("../../config.json", 'r')
-configuration = json.loads(config_file.read())["tray"]
+configuration = json.loads(config_file.read())["food_tray"]
 print(configuration)
-
-motor_moving = False
 
 
 def setup():
 	# setting up the RaspberryPi modes
 	gpio.setwarnings(False)  # Ignore warning for now
 	gpio.setmode(gpio.BCM)  # Use physical pin numbering
+
+	# setting up Ultrasonic sensor
+	gpio.setup(configuration['ultrasonic_trigger'], gpio.OUT)
+	gpio.setup(configuration['ultrasonic_echo'], gpio.IN)
 
 	# setting up the motor 1
 	gpio.setup(configuration['motor_1_direction_pin'], gpio.OUT)
@@ -28,25 +31,53 @@ def setup():
 	gpio.setup(configuration['top_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # top switch
 	gpio.setup(configuration['bottom_switch'], gpio.IN, pull_up_down=gpio.PUD_DOWN)  # bottom Switch
 
+def distance():
+	# set Trigger to HIGH
+	gpio.output(configuration['ultrasonic_trigger'], True)
 
-def motor_rotate():
-	gpio.output(configuration['motor_1_step_pin'], gpio.HIGH)
-	gpio.output(configuration['motor_2_step_pin'], gpio.HIGH)
-	sleep(.0002)
-	gpio.output(configuration['motor_1_step_pin'], gpio.LOW)
-	gpio.output(configuration['motor_2_step_pin'], gpio.LOW)
-	sleep(.0002)
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	gpio.output(configuration['ultrasonic_trigger'], False)
+
+	StartTime = time.time()
+	StopTime = time.time()
+
+	# save StartTime
+	while gpio.input(configuration['ultrasonic_echo']) == 0:
+		StartTime = time.time()
+
+	# save time of arrival
+	while gpio.input(configuration['ultrasonic_echo']) == 1:
+		StopTime = time.time()
+
+	# time difference between start and arrival
+	TimeElapsed = StopTime - StartTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	distance = (TimeElapsed * 34300) / 2
+
+	return distance
+
+
+def motor_rotate(pause=False):
+	if not pause:
+		gpio.output(configuration['motor_1_step_pin'], gpio.HIGH)
+		gpio.output(configuration['motor_2_step_pin'], gpio.HIGH)
+		sleep(.0002)
+		gpio.output(configuration['motor_1_step_pin'], gpio.LOW)
+		gpio.output(configuration['motor_2_step_pin'], gpio.LOW)
+		sleep(.0002)
 
 
 def start_condition():
-	if gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
+	if gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
 		return True
 	else:
 		return False
 
 
 def stop_condition():
-	if gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
+	if gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
 		return False
 	else:
 		return True
@@ -54,7 +85,8 @@ def stop_condition():
 
 if __name__ == '__main__':
 	setup()
+	print("setup completed")
 	while True:
 		while start_condition():
 			while stop_condition():
-				motor_rotate()
+				motor_rotate(pause=False)
