d062347780dadb2fd1caba0682b0764298498115
rishabh k sharma <rishabh@warehouseoptimization.com>
2020-09-30 14:12:00 +0530

    food tray work

--- temp_codes/remote-control/food_tray.py
+++ temp_codes/remote-control/food_tray.py
@@ -63,6 +63,7 @@ while True:
 	if operating_mode == 'use':
 		gpio.output(configuration['motor_1_direction_pin'], configuration["direction_up"])
 		gpio.output(configuration['motor_2_direction_pin'], configuration["direction_up"])
+		motor_rotate(rotate=True)
 		if gpio.input(configuration['top_switch']) == gpio.HIGH:
 			motor_rotate(rotate=False)
 			operating_mode = ''
