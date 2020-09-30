26f604d0cee66dfd83651d7f84a71d3f3cf7e9c6
rishabh k sharma <rishabh@warehouseoptimization.com>
2020-09-30 13:37:11 +0530

    adding cleanup

--- temp_codes/remote-control/food_tray.py
+++ temp_codes/remote-control/food_tray.py
@@ -47,21 +47,22 @@ while True:
 		operating_mode = 'use'
 	if gpio.input(configuration['refill']):
 		operating_mode = 'refill'
-
-	if operating_mode == 'refill':
-		gpio.output(configuration['motor_1_direction_pin'], configuration["direction_down"])
-		gpio.output(configuration['motor_2_direction_pin'], configuration["direction_down"])
-		while gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
-			while gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
-				if not gpio.input(configuration['IR_sensor']):
-					motor_rotate()
-
-	if operating_mode == 'use':
-		gpio.output(configuration['motor_1_direction_pin'], configuration["direction_up"])
-		gpio.output(configuration['motor_2_direction_pin'], configuration["direction_up"])
-		while gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(
-				configuration['bottom_switch']) == gpio.HIGH:
-			while gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(
-					configuration['bottom_switch']) == gpio.LOW:
-				if gpio.input(configuration['IR_sensor']):
-					motor_rotate()
+		
+	print(operating_mode)
+	# if operating_mode == 'refill':
+	# 	gpio.output(configuration['motor_1_direction_pin'], configuration["direction_down"])
+	# 	gpio.output(configuration['motor_2_direction_pin'], configuration["direction_down"])
+	# 	while gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(configuration['bottom_switch']) == gpio.LOW:
+	# 		while gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(configuration['bottom_switch']) == gpio.HIGH:
+	# 			if not gpio.input(configuration['IR_sensor']):
+	# 				motor_rotate()
+	#
+	# if operating_mode == 'use':
+	# 	gpio.output(configuration['motor_1_direction_pin'], configuration["direction_up"])
+	# 	gpio.output(configuration['motor_2_direction_pin'], configuration["direction_up"])
+	# 	while gpio.input(configuration['top_switch']) == gpio.LOW and gpio.input(
+	# 			configuration['bottom_switch']) == gpio.HIGH:
+	# 		while gpio.input(configuration['top_switch']) == gpio.HIGH and gpio.input(
+	# 				configuration['bottom_switch']) == gpio.LOW:
+	# 			if gpio.input(configuration['IR_sensor']):
+	# 				motor_rotate()
