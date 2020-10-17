ed0388e0a762e69d4a94f4a9a3e32a96bca423c4
rishabh-as <Predict@123>
2020-10-17 10:40:46 +0530

    flask app

---
+++ temp_codes/bldc.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as gpio
+from time import sleep
+
+configuration = {"bldc": {
+	"PWM1": 12,
+	"startStop1": 1,
+	"Direction1": 26,
+	"PWM2": 13,
+	"startStop2": 0,
+	"Direction2": 4
+}}
+
+gpio.setwarnings(False)  # Ignore warning for now
+gpio.setmode(gpio.BCM)  # Use physical pin numbering
+
+# setting up the motor 1
+gpio.setup(configuration['PWM1'], gpio.OUT)
+gpio.setup(configuration['startStop1'], gpio.OUT)
+gpio.setup(configuration['Direction1'], gpio.OUT)
+pi_pwm1 = gpio.PWM(configuration['PWM1'], 1000)
+pi_pwm1.start(20)
+
+while true:
+	pi_pwm1.ChangeDutyCycle(55)
